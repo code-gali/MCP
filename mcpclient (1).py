c278daed3869168f9c647ef906a1d515b482de6c
code-gali <bharadwaj.connect@gmail.com>
2025-05-12 15:45:18 -0400

    Add files via upload

---
+++ mcpclient (1).py
@@ -0,0 +1,83 @@
+from mcp import (
+    ClientSession, 
+    StdioServerParameters,  
+)
+from mcp.client.stdio import stdio_client
+from mcp.client.sse import sse_client
+
+# Create server parameters for stdio connection
+server_params = StdioServerParameters(
+    command="python",  # Executable
+    args=["mcpserver.py"],  # Optional command line arguments
+    env=None,  # Optional environment variables
+)
+
+async def run():
+    async with stdio_client(server_params) as (read, write):
+        async with ClientSession(
+            read, write
+        ) as session:
+            # Initialize the connection
+            await session.initialize()
+
+            # List available resources
+            resources = await session.list_resources()
+            print(resources)
+            # List available tools
+            tools = await session.list_tools()
+            print(tools)
+            # Read a resource
+            #content, mime_type = await session.read_resource("search://cortex_search/search_obj/list")
+
+async def sse_run():
+    async with sse_client(url='http://0.0.0.0:8000/sse') as sse_connection:
+        async with ClientSession(
+            *sse_connection
+        ) as session:
+            
+            # Initialize the connection
+            await session.initialize()
+            
+            # List available resources
+            resources = await session.list_resources()
+            print(resources)
+            
+            resources = await session.list_resource_templates()
+            print(resources)
+
+            tools = await session.list_tools()
+            print(tools)
+            
+            prompts = await session.list_prompts()
+            print(prompts)
+            
+            #Read a resource
+            content = await session.read_resource("genaiplatform://hedis/frequent_questions/Initialization")
+            print(content)
+            await session.call_tool(
+                name="add-frequent-questions",
+                arguments={
+                    "uri": "genaiplatform://hedis/frequent_questions/Initialization",
+                    "questions": [
+                        {
+                            "user_context": "Initialization",
+                            "prompt": "What is the age criteria for CBP Measure?"
+                        }
+                    ]
+                }
+            )
+            content = await session.read_resource("genaiplatform://hedis/frequent_questions/Initialization")
+            print(content)
+            #await session.complete()
+            
+            #content = await session.read_resource("schematiclayer://cortex_analyst/schematic_models/hedis_stage_full/list")
+            #print(content.contents)
+
+            
+
+
+            
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(sse_run())
